c641a778285cd0134a25ed05f0fcb566fba4f395
sodiel <detopuse@gmail.com>
2025-03-27 18:15:31 +0300

    fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,11 +10,11 @@ function App() {
   const user = window.Telegram.WebApp.initDataUnsafe?.user; // Get user data
 
   return (
-    <div className="bg-cover bg-secondary-bg  text-text h-viewport flex flex-col p-4">
+    <div className="bg-secondary-bg  text-text flex flex-col p-4">
       <h1 className="text-3xl font-bold justify-start items-start bg-background">
         Hello, {user?.first_name || "Guest"}!
       </h1>
-      <div className="flex flex-col p-4 m-10 justify-center items-center ">
+      <div className="flex flex-col p-4 m-10 justify-center items-center">
         <Card
           name="Анатолий"
           age={23}
@@ -31,14 +31,14 @@ function App() {
         ></Card>
         <Card
           name="Алексей"
-          age={223}
+          age={17}
           avatarURL={
             "https://tr.rbxcdn.com/180DAY-d2aa8b558f4c73dc77ab184210a56788/420/420/Hat/Png/noFilter"
           }
         ></Card>
         <Card
-          name="Анатолий"
-          age={23}
+          name="Антон"
+          age={41}
           avatarURL={
             "https://tr.rbxcdn.com/180DAY-d2aa8b558f4c73dc77ab184210a56788/420/420/Hat/Png/noFilter"
           }
